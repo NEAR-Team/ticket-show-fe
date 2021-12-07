51279d541fd58516fcccb2f92da4075bafaf40f7
Leo Pham <hongthaipro@gmail.com>
2021-12-07 15:40:02 +0700

    AppProvider connect contract account

---
+++ context/app.context.js
@@ -0,0 +1,58 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { connect, Contract, keyStores, WalletConnection } from 'near-api-js'
+
+import getConfig from "../config/near.config";
+
+const nearConfig = getConfig(process.env.NODE_ENV || 'development');
+
+const AppContext = createContext();
+export const useAppContext = () => useContext(AppContext);
+
+const AppProvider = ({ children }) => {
+
+
+    const [isAuth, setIsAuth] = useState(false);
+    const [account, setAccount] = useState(null);
+    const [contract, setContract] = useState(null);
+    const [walletConnection, setWalletConnection] = useState(null);
+
+    const initNear = async () => {
+        const near = await connect(Object.assign({ deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() } }, nearConfig))
+        const walletConnection = new WalletConnection(near)
+        const contract = await new Contract(walletConnection.account(), nearConfig.contractName, {
+            viewMethods: ['getGreeting'],
+            changeMethods: ['setGreeting'],
+        })
+
+        return { walletConnection, accountId: walletConnection.getAccountId(), contract };
+    }
+
+    const login = () => {
+
+        walletConnection.requestSignIn(nearConfig.contractName)
+    }
+
+    const logout = () => {
+        walletConnection.signOut();
+        window.location.replace('/');
+    }
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            initNear().then(({ walletConnection, accountId, contract }) => {
+                setWalletConnection(walletConnection);
+                if (walletConnection.isSignedIn()) {
+                    setIsAuth(true);
+                    setAccount(accountId);
+                    setContract(contract);
+
+                }
+            })
+        }
+    }, [])
+
+
+    return <AppContext.Provider value={{ networkId: nearConfig.networkId, isAuth, login, logout, account, contract }}>{children}</AppContext.Provider>;
+};
+
+export default AppProvider;
